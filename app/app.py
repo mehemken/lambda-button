df40e33653826c7b73ceff83f7891f579130c387
Marco Hemken <mehemken@gmail.com>
2017-06-01 08:06:08 -0700

    submit button in the form works

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+
+class LambdaButton(FlaskForm):
+    pass
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'reallybadpassword'
 
-@app.route('/')
+@app.route('/', methods=('GET','POST'))
 def index():
-    return render_template('index.html')
+    form = LambdaButton()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
