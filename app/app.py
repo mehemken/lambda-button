6a17ca748b979feb398e9361e045932d41532b17
Marco Hemken <mehemken@gmail.com>
2017-06-10 10:38:17 -0700

    final version

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
+from message import ping_lambda
 
 class LambdaButton(FlaskForm):
     pass
@@ -11,8 +12,17 @@ app.config['SECRET_KEY'] = 'reallybadpassword'
 def index():
     form = LambdaButton()
     if form.validate_on_submit():
-        return redirect('/')
+        new_value = ping_lambda()
+        return redirect(f'/{new_value}')
     return render_template('index.html', form=form)
+
+@app.route('/<value>', methods=('GET','POST'))
+def new(value):
+    form = LambdaButton()
+    if form.validate_on_submit():
+        new_value = ping_lambda()
+        return redirect(f'/{new_value}')
+    return render_template('index.html', form=form, value=value)
 
 if __name__ == '__main__':
     app.run(debug=True)
